5969a3c1901060b1b3f95e3dd63f576a6fa4674a
Artur Pędziwiatr <62343144+ArtekPedz@users.noreply.github.com>
2020-04-30 21:10:42 +0200

    Add files via upload

---
+++ Saper/FormMainWindows.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Saper
+{
+    partial class FormMainWindows
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menuglowne = new System.Windows.Forms.MenuStrip();
+            this.grajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.prostaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.śrdniozaawansowanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trudnaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.Menuglowne.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menuglowne
+            // 
+            this.Menuglowne.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Menuglowne.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grajToolStripMenuItem});
+            this.Menuglowne.Location = new System.Drawing.Point(0, 0);
+            this.Menuglowne.Name = "Menuglowne";
+            this.Menuglowne.Size = new System.Drawing.Size(800, 28);
+            this.Menuglowne.TabIndex = 0;
+            this.Menuglowne.Text = "menuStrip1";
+            // 
+            // grajToolStripMenuItem
+            // 
+            this.grajToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.prostaToolStripMenuItem,
+            this.śrdniozaawansowanaToolStripMenuItem,
+            this.trudnaToolStripMenuItem});
+            this.grajToolStripMenuItem.Name = "grajToolStripMenuItem";
+            this.grajToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.grajToolStripMenuItem.Text = "Graj";
+            // 
+            // prostaToolStripMenuItem
+            // 
+            this.prostaToolStripMenuItem.Name = "prostaToolStripMenuItem";
+            this.prostaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.prostaToolStripMenuItem.Text = "Łatwa";
+            this.prostaToolStripMenuItem.Click += new System.EventHandler(this.prostaToolStripMenuItem_Click);
+            // 
+            // śrdniozaawansowanaToolStripMenuItem
+            // 
+            this.śrdniozaawansowanaToolStripMenuItem.Name = "śrdniozaawansowanaToolStripMenuItem";
+            this.śrdniozaawansowanaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.śrdniozaawansowanaToolStripMenuItem.Text = "Srednia";
+            // 
+            // trudnaToolStripMenuItem
+            // 
+            this.trudnaToolStripMenuItem.Name = "trudnaToolStripMenuItem";
+            this.trudnaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.trudnaToolStripMenuItem.Text = "Trudna";
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.AutoSize = true;
+            this.panelButtons.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelButtons.Location = new System.Drawing.Point(0, 28);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(800, 422);
+            this.panelButtons.TabIndex = 1;
+            // 
+            // FormMainWindows
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.Menuglowne);
+            this.MainMenuStrip = this.Menuglowne;
+            this.Name = "FormMainWindows";
+            this.Text = "Saper v1.0";
+            this.Load += new System.EventHandler(this.FormMainWindows_Load);
+            this.Menuglowne.ResumeLayout(false);
+            this.Menuglowne.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Menuglowne;
+        private System.Windows.Forms.ToolStripMenuItem grajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem prostaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem śrdniozaawansowanaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trudnaToolStripMenuItem;
+        private System.Windows.Forms.Panel panelButtons;
+    }
+}
+
---
+++ Saper/FormMainWindows.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saper
+{
+    public partial class FormMainWindows : Form
+    {
+        private const int fieldSize = 30;
+        private SapperLogic myGame;
+        public FormMainWindows()
+        {
+            InitializeComponent();
+            prostaToolStripMenuItem_Click(null, null);
+        }
+        private void prostaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myGame = new SapperLogic(8, 8, 10);
+            generateView();
+        }
+
+        private void srdnioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myGame = new SapperLogic(12, 10, 25);
+            generateView();
+        }
+
+        private void trudnaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myGame = new SapperLogic(20, 15, 50);
+            generateView();
+        }
+
+        private void generateView()
+        {
+            panelButtons.Controls.Clear();
+
+            for (int x = 0; x < myGame.BoardWidth; x++)
+            {
+                for (int y = 0; y < myGame.BoardHeight; y++)
+                {
+                    Button b = new Button();
+                    b.Size = new Size(fieldSize, fieldSize);
+                    b.Location = new Point(fieldSize * x, fieldSize * y);
+                    b.Click += button_Click;
+                    panelButtons.Controls.Add(b);
+                    b.Tag = new Point(x, y);
+                }
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if(myGame.State == SapperLogic.GameState.InProgress)
+            {
+                if(sender is Button)
+                {
+                    Button b = sender as Button;
+                    if(b.Tag is Point)
+                    {
+                        Point p = (Point)b.Tag;
+
+                        myGame.Uncover(p);
+                        refreshView();
+
+                        if(myGame.State == SapperLogic.GameState.Win)
+                        {
+                            MessageBox.Show("Nie najgorzej stary :)");
+                        }
+                        if (myGame.State == SapperLogic.GameState.Loss)
+                        {
+                            MessageBox.Show("Sorry bro, przegrałeś");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void refreshView()
+        {
+            foreach(Button b in panelButtons.Controls)
+            {
+                SapperLogic.Field f = myGame.GetField((Point)b.Tag);
+                if(f.Covered == false)
+                {
+                    if(f.FieldType == SapperLogic.FieldTypeEnum.Bomb)
+                    {
+                        b.BackColor = Color.Red;
+                        b.Text = "@";
+                    }
+                    else
+                    {
+                        b.BackColor = Color.White;
+
+                        if(f.FieldType == SapperLogic.FieldTypeEnum.BombCount)
+                        {
+                            b.Text = f.BombCount.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FormMainWindows_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class SapperLogic
+    {
+        internal enum FieldTypeEnum
+        {
+            Bomb,
+            BombCount,
+            Empty
+        }
+        internal enum GameState
+        {
+            InProgress,
+            Win,
+            Loss
+        }
+        internal class Field
+        {
+            private FieldTypeEnum fieldType;
+            private int bombCount;
+            private bool covered;
+
+            public FieldTypeEnum FieldType { get { return fieldType; } }
+            public int BombCount { get { return bombCount; } }
+            public bool Covered { get { return covered; } set { covered = value; } }
+
+            internal Field(FieldTypeEnum fieldType, int bombCount = 0)
+            {
+                this.covered = true;
+                this.fieldType = fieldType;
+                this.bombCount = bombCount;
+            }
+        }
+
+        readonly Random generator = new Random();
+        private Field[,] board;
+        private int fieldsToUncover;
+        private GameState state;
+
+        public int BoardWidth
+        {
+            get { return (int)board.GetLongLength(0); }
+        }
+        public int BoardHeight
+        {
+            get { return (int)board.GetLongLength(0); }
+        }
+        public GameState State
+        {
+            get { return state; }
+        }
+
+        public SapperLogic(int width, int height, int bombCount)
+        {
+            this.fieldsToUncover = (width * height) - bombCount;
+            this.board = new Field[width, height];
+
+            do
+            {
+                int x = generator.Next(BoardWidth);
+                int y = generator.Next(BoardHeight);
+                if(board[x,y] == null)
+                {
+                    board[x, y] = new Field(FieldTypeEnum.Bomb);
+                    bombCount--;
+                }
+            } while (bombCount > 0);
+
+            for (int x = 0; x < BoardWidth; x++)
+            {
+                for(int y = 0; y < BoardHeight; y++)
+                {
+                    if(board[x,y] == null)
+                    {
+                        int localBombCount = 0;
+                        for (int xx = x-1; xx<=x+1; xx++)
+                        {
+                            for(int yy = y-1; yy<=y+1; yy++)
+                            {
+                                if(xx>=0 && xx<BoardWidth && yy>=0 && yy<BoardHeight && board[xx,yy]!=null && board[xx,yy].FieldType == FieldTypeEnum.Bomb)
+                                {
+                                    localBombCount++;
+                                }
+                            }
+                        }
+                        if (localBombCount > 0) board[x, y] = new Field(FieldTypeEnum.BombCount, localBombCount);
+                        else  board[x, y] = new Field(FieldTypeEnum.Empty);
+
+                    }
+                }
+            }
+        }
+
+        internal Field GetField(Point p)
+        {
+            return board[p.X, p.Y];
+        }
+
+        internal void Uncover(Point p)
+        {
+            Field f = board[p.X, p.Y];
+            if(f.Covered)
+            {
+                if(f.FieldType == FieldTypeEnum.Bomb)
+                {
+                    state = GameState.Loss;
+                    for(int x=0; x<BoardWidth; x++)
+                    {
+                        for(int y=0; y<BoardHeight; y++)
+                        {
+                            board[x, y].Covered = false;
+                        }
+                    }
+                }
+                else if (f.FieldType == FieldTypeEnum.BombCount)
+                {
+                    f.Covered = false;
+                    fieldsToUncover--;
+                }
+                else
+                {
+                    f.Covered = false;
+                    fieldsToUncover--;
+                    for(int xx = p.X-1; xx<=p.X+1; xx++)
+                    {
+                        for(int yy = p.Y-1; yy<=p.Y+1; yy++)
+                        {
+                            if(xx >= 0 && xx < BoardWidth && yy >= 0 && yy<BoardHeight)
+                            {
+                                Uncover(new Point(xx, yy));
+                            }
+                        }
+                    }
+                }
+
+                if(fieldsToUncover == 0)
+                {
+                    state = GameState.Win;
+                }
+            }
+        }
+
+    }
+    
+}
